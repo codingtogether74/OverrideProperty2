7da39e848cdc8e1d0bbdaaada522a9b43b8af472
BestKora <developer@bestkora.com>
2014-10-18 20:22:08 +0400

    EveryThing works

--- OverrideProperty2/ViewController.swift
+++ OverrideProperty2/ViewController.swift
@@ -15,18 +15,6 @@ class ViewController: UIViewController {
                             
     @IBOutlet var contentsLabel: UILabel?
   
-    struct Blog : Printable {
-        let id: Int = 30
-        let name: String = "mm"
-        let needsPassword : Bool = true
-        let url: NSURL = NSURL(string:"http://flickrtest1.userland.com/")
-        var description : String {
-            return "Blog { id = \(id), name = \(name), needsPassword = \(needsPassword), url = \(url)}"
-        }
-        
-    }
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -37,7 +25,7 @@ class ViewController: UIViewController {
         var card2Before = pcard2.contents
         pcard2.rank = 10
        var card2After = pcard2.contents
-//        contentsLabel!.text = card2Before + card2After
+        contentsLabel!.text = card2Before + card2After
         
     }
 }
